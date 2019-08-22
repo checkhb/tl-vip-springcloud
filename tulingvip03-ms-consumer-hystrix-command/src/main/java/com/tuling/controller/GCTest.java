4231eb0b16031b10f5dcf2c47800cc5598643a50
hubin <503693631@qq.com>
2019-08-22 15:47:01 +0800

    hystrix服务熔断,降级

--- tulingvip03-ms-consumer-hystrix-command/src/main/java/com/tuling/controller/GCTest.java
+++ tulingvip03-ms-consumer-hystrix-command/src/main/java/com/tuling/controller/GCTest.java
@@ -1,4 +1,14 @@
 package com.tuling.controller;
 
 public class GCTest {
+    public static void main(String[] args) {
+       byte[] allocation1, allocation2, allocation3, allocation4/*, allocation5, allocation6*/;
+        allocation1 = new byte[28000 * 1024];
+        allocation2 = new byte[9000 * 1024];
+        allocation3 = new byte[10000 * 1024];
+        allocation4 = new byte[10000 * 1024];
+        /*allocation5 = new byte[1000 * 1024];
+        allocation5 = new byte[1000 * 1024];*/
+
+    }
 }
--- tulingvip03-ms-consumer-hystrix-command/src/main/java/com/tuling/controller/User.java
+++ tulingvip03-ms-consumer-hystrix-command/src/main/java/com/tuling/controller/User.java
@@ -8,6 +8,12 @@ public class User {
     private String name;
 
     public User(int id, String name) {
-
+        this.id = id;
+        this.name = name;
     }
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("关闭资源，user"+ id + "即将被回收");
+    }
+
 }
